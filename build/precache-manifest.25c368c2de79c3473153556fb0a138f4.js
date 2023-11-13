9ae2701d4f58b144338a017f5eae99638c7fda41
MUHAMMAD SALMAN AHMED <73326171+salman-2244@users.noreply.github.com>
2023-11-13 21:02:32 +0100

    Add files via upload

---
+++ build/precache-manifest.25c368c2de79c3473153556fb0a138f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158475c4bd8398afdb3db390afd6d142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "612fa894319c3ec4b08c",
+    "url": "/static/css/main.f0ba070e.chunk.css"
+  },
+  {
+    "revision": "47e448d44a29bc8e8eec",
+    "url": "/static/js/2.2ad8640c.chunk.js"
+  },
+  {
+    "revision": "612fa894319c3ec4b08c",
+    "url": "/static/js/main.badd3937.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
